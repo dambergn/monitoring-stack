4a6f3abe05102ca79311e0d7cde6d814ce7b0cf8
dambergn <dambergn@gmail.com>
2025-07-10 09:30:28 -0700

    ading tempo config to stack

--- restart.sh
+++ restart.sh
@@ -5,25 +5,31 @@
 # Restart Grafana
 cd grafana
 docker compose down
-docker compose up -d
+docker compose up -d --force-recreate
 cd ..
 
 # Restart Prometheus
 cd prometheus
 docker compose down
-docker compose up -d
+docker compose up -d --force-recreate
 cd ..
 
 # Restart Alloy
 cd alloy
 docker compose down
-docker compose up -d
+docker compose up -d --force-recreate
 cd ..
 
 # Restart Loki
 cd loki
 docker compose down
-docker compose up -d
+docker compose up -d --force-recreate
+cd ..
+
+# Restart Tempo
+cd tempo
+docker compose down
+docker compose up -d --force-recreate
 cd ..
 
 echo "*** Restart Complete ***"
