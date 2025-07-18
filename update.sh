17aa1f98e3e4369894b44be9b106dd9c457cb5c4
dambergn <dambergn@gmail.com>
2025-07-18 12:22:59 -0700

    update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+docker compose down
 ## Check for updates.
+docker compose pull
 
 ## only restart those that were updated.
+docker compose up --build --force-recreate -d
 
 ## Clean up old files.
